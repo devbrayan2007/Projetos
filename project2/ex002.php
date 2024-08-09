d11e98e1dbb544175ba2e053196fe9545693a8d2
devbrayan2007 <devbrayan2007@gmail.com>
2024-08-09 17:44:48 -0300

    update

--- project2/ex002.php
+++ project2/ex002.php
@@ -1,2 +1,8 @@
 <?php
 
+$note1 = $_POST['note1'];
+$note2 = $_POST['note2'];
+$note3 = $_POST['note3'];
+
+$medium = ($note1 + $note2 + $note3) / 3;
+echo "Average obtained: " . round($medium);
---
+++ project3/ex003.php
@@ -0,0 +1,6 @@
+<?php
+
+$salary = $_POST['salary'];
+$salary_calculation = $salary * 0.15;
+
+echo "Your salary with 15% descont is: $salary_calculation";
